81d7a12c6e241f3a1a6f2875f63f2c2ed463f89a
Shivank Pandey <125184309+Shivankk-Pandeyy@users.noreply.github.com>
2024-06-14 13:57:46 +0530

    Update Connection.js

--- Connection/Connection.js
+++ Connection/Connection.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 const MongoPass=process.env.MONGO_PASS;
 const ConnectDb=async()=>{
     try{
-        await mongoose.connect(`mongodb+srv://pandeyshivank21:${MongoPass}@cluster0.gnqrx03.mongodb.net/expense?retryWrites=true&w=majority&appName=Cluster0`);
+        await mongoose.connect(`mongodb+srv://pandeyshivank21:${MongoPass}@cluster0.gnqrx03.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
         console.log("MongoDB Connected");
     }
     catch(err){
