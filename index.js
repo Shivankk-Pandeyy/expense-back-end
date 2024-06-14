564216f823c77be2fddd2c0f9fcf4e0998c42730
Shivank Pandey <125184309+Shivankk-Pandeyy@users.noreply.github.com>
2024-06-14 16:23:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const product_routes=require("./Routes/Product");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*"); // Specify your client domain
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // Specify your client domain
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 });
 
 app.options('*', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
