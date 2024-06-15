245f35a9e6074245f89a8845c9e360237c2031cc
Shivank Pandey <125184309+Shivankk-Pandeyy@users.noreply.github.com>
2024-06-15 12:05:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ const product_routes=require("./Routes/Product");
 //MIDDLEWARE
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 app.use(cors(
     {
         origin:"https://expense-front-end.vercel.app",
