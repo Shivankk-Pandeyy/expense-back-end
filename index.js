13dd2d596395da7ea617bfba6bfde45de486edcd
Shivank Pandey <125184309+Shivankk-Pandeyy@users.noreply.github.com>
2024-06-15 12:06:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const product_routes=require("./Routes/Product");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "expense-front-end.vercel.app");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
